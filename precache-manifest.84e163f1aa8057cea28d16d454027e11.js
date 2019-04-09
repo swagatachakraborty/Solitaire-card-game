f9e372961cbd562d0679719eaa40069bbfa87e4c
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-04-09 21:00:19 +0530

    Updates

---
+++ precache-manifest.84e163f1aa8057cea28d16d454027e11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcaee55cf977ceea331",
+    "url": "/Solitaire-card-game/static/js/runtime~main.335b2b13.js"
+  },
+  {
+    "revision": "51b88c0289d9c5df0486",
+    "url": "/Solitaire-card-game/static/js/main.e8b70134.chunk.js"
+  },
+  {
+    "revision": "7b3039637046a48367db",
+    "url": "/Solitaire-card-game/static/js/2.409743f3.chunk.js"
+  },
+  {
+    "revision": "51b88c0289d9c5df0486",
+    "url": "/Solitaire-card-game/static/css/main.d6be7d05.chunk.css"
+  },
+  {
+    "revision": "9423663ad295b327b2fe6b00a58de8c7",
+    "url": "/Solitaire-card-game/index.html"
+  }
+];
